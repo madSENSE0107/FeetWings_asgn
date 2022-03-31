83408635bd6caeb9b67d93a81ec4755d2744ceec
madSENSE0107 <saptarsi2000@gmail.com>
2022-03-31 20:07:44 +0530

    options to chose added

--- src/CreateCareer.js
+++ src/CreateCareer.js
@@ -1,7 +1,70 @@
-
+import React, {  useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
 export default function CreateCareer() {
+  const [editor] = useState(() => withReact(createEditor()));
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
+
+  const employment_type = [
+    "Full Time",
+    "Part Time",
+    "Contract",
+    "Temporary",
+    "Voluntary",
+    "Internship",
+  ];
+  const Job_Section = [
+    "Business Systems",
+    "Design",
+    "Engineering",
+    "Finance",
+    "Human Resource",
+    "Legal",
+    "Marketting",
+    "Operations",
+    "Product",
+    "Sales",
+    "Security",
+    "Support",
+  ];
+  const workPlace = ["On-site", "Hybrid", "Remote"];
+  const options_types = employment_type.map((i) => (
+    <option value={i}>{i}</option>
+  ));
+  const job_types = Job_Section.map((i) => <option value={i}>{i}</option>);
+  const workPlace_type = workPlace.map((i) => <option value={i}>{i}</option>);
   return (
-    <div>create_career</div>
-  )
+    <>
+      <h1>Career Dashboard</h1>
+      <form>
+        <input type="value" placeholder="Title" />
+        <select name="Employment Type" placeholder="Title">
+          {options_types}
+        </select>
+        <input type="value" placeholder="Location" />
+        <select name="Job Section">{job_types}</select>
+        <select name="WorkPlace Options">{workPlace_type}</select>
+        <select name="WorkPlace Options">
+          <option value="Paid">Paid</option>
+          <option value="Gratis">Gratis</option>
+        </select>
+        <h3>Add a description</h3>
+        <div>
+          <Slate
+            editor={editor}
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+          >
+            <Editable />
+          </Slate>
+        </div>
+      </form>
+    </>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import "./navbar.css"
 
 function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className="navbar">
           <div>Logo</div>
           <div>
-              <div>
-                  <li><Link to="/">Our Products</Link></li>
-                  <li><Link to="/">Insights</Link></li>
-                  <li><Link to="/">Smart Yarn</Link></li>
-                  <li><Link to="/">About us</Link></li>
-                  <li><Link to="/">Contact</Link></li>
+              <div className="navbar_links"> 
+                  <li className="links"><Link to="/">Our Products</Link></li>
+                  <li className="links"><Link to="/">Insights</Link></li>
+                  <li className="links"><Link to="/">Smart Yarn</Link></li>
+                  <li className="links"><Link to="/">About us</Link></li>
+                  <li className="links"><Link to="/">Contact</Link></li>
               </div>
           </div>
       </nav>
